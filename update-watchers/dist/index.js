e3ba50f2ea45930aa998138d812b0e35d4818c0a
Tom Stellard <tstellar@redhat.com>
2020-02-03 23:57:20 +0000

    Mention users directly

--- update-watchers/dist/index.js
+++ update-watchers/dist/index.js
@@ -495,14 +495,28 @@ async function run() {
   const octokit = new github.GitHub(token);
 
   const issueID = github.context.issue.number;
-  // Need to put a space at the end for this to be considered a mention.
-  const teamname = "@llvm/issue-subscribers-" + github.context.payload.label.name.replace(/ /g, "-") + " "
+  const teamname = "issue-subscribers-" + github.context.payload.label.name.replace(/ /g, "-")
+
+  mentionList = ''
+
+  // GitHub actions can't mention teams, because it's not a member of the
+  // organization, so it has to mention people directly.
+  const userList = octokit.teams.listMembersInOrg.endpoint.merge({
+    org: 'llvm',
+    team_slug : teamname
+  })
+
+  octokit.paginate(userList).then(users => {
+
+  users.forEach(user => mentionList += '@' + user + ' ')
+  // issues is an array of all issue objects
+});
 
    octokit.issues.createComment({
      owner: 'llvm',
      repo: 'temp-issue-tester',
      issue_number: issueID,
-     body: teamname,
+     body: mentionList,
    })
 }
 
