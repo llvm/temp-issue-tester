21aa3b63db52fa2dec30acda8f9b021ba80abd58
Tom Stellard <tstellar@redhat.com>
2023-09-12 21:28:44 -0700

    Fix typo and add debugging

--- .github/workflows/pr-subscriber-wait.py
+++ .github/workflows/pr-subscriber-wait.py
@@ -5,9 +5,11 @@ import sys
 def needs_to_wait(repo):
 
     workflow_name = os.environ.get("GITHUB_WORKFLOW")
-    run_number = os.environ.get("GITHUB_RUN_NUBMER")
+    run_number = os.environ.get("GITHUB_RUN_NUMBER")
+    print("Workflow Name:", workflow_name, "Run Number:", run_number)
     for status in ["in_progress", "queued"]:
         for workflow in repo.get_workflow_runs(status = status):
+            print("Looking at ", workflow.name, "#", workflow.run_number)
             if workflow.name != workflow_name:
                 continue
             if workflow.run_number < run_number:
