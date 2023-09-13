0060627f7b5d01cd0ae61c39517333d575dcd9ef
Tom Stellard <tstellar@redhat.com>
2023-09-12 20:49:32 -0700

    Try using script

---
+++ .github/workflows/pr-subscriber-wait.py
@@ -0,0 +1,22 @@
+import github
+import os
+import sys
+
+def needs_to_wait(repo):
+
+    workflow_name = os.environ.get("GITHUB_WORKFLOW")
+    run_number = os.environ.get("GITHUB_RUN_NUBMER")
+    for status in ["in_progress", "queued"]:
+        for workflow in repo.get_workflow_runs(status = status):
+            if workflow.name != workflow_name:
+                continue
+            if workflow.run_number < run_number:
+                print("Workflow {} still {} ".format(workflow.run_number, status))
+                return True
+    return False
+
+repo_name = os.environ.get("GITHUB_REPOSITORY")
+token = sys.argv[1]
+repo = github.Github(token).get_repo(repo_name)
+while needs_to_wait(repo):
+    time.sleep(60)
